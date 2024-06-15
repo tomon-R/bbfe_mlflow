287c68735e7adb4bd327e8e00621d32d1b79d091
kwkm0429 <kwkm0429@gmail.com>
2024-06-15 12:46:00 +0900

    add fluid elemmat functions for crank nicolson

--- FE_solvers/mlflow_fs_sups/fluid_elemmat.c
+++ FE_solvers/mlflow_fs_sups/fluid_elemmat.c
@@ -152,3 +152,140 @@ void BBFE_elemmat_fluid_sups_vec(
 
 	vec[3] = tau * BB_calc_vec3d_dot(grad_N_i, v);
 }
+
+/**********************************************************
+ * Crank-Nicolson version
+ **********************************************************/
+void BBFE_elemmat_fluid_sups_mat_crank_nicolson(
+		double         mat[4][4],
+		const double   N_i,
+		const double   N_j,
+		const double   grad_N_i[3],
+		const double   grad_N_j[3],
+		const double   v[3],
+		const double   density,
+		const double   viscosity,
+		const double   tau,
+		const double   dt)
+{
+	double M = density * N_i * N_j;
+	double A = dt * density * N_i * BB_calc_vec3d_dot(v, grad_N_j);
+
+	double G1 = - dt * grad_N_i[0] * N_j;
+	double G2 = - dt * grad_N_i[1] * N_j;
+	double G3 = - dt * grad_N_i[2] * N_j;
+
+	double D_11 = dt * viscosity * ( BB_calc_vec3d_dot(grad_N_i, grad_N_j) + grad_N_i[0]*grad_N_j[0] );
+	double D_12 = dt * viscosity * grad_N_i[1] * grad_N_j[0];
+	double D_13 = dt * viscosity * grad_N_i[2] * grad_N_j[0];
+	double D_21 = dt * viscosity * grad_N_i[0] * grad_N_j[1];
+	double D_22 = dt * viscosity * ( BB_calc_vec3d_dot(grad_N_i, grad_N_j) + grad_N_i[1]*grad_N_j[1] );
+	double D_23 = dt * viscosity * grad_N_i[2] * grad_N_j[1];
+	double D_31 = dt * viscosity * grad_N_i[0] * grad_N_j[2];
+	double D_32 = dt * viscosity * grad_N_i[1] * grad_N_j[2];
+	double D_33 = dt * viscosity * ( BB_calc_vec3d_dot(grad_N_i, grad_N_j) + grad_N_i[2]*grad_N_j[2] );
+
+	double C1 = dt * N_i * grad_N_j[0];
+	double C2 = dt * N_i * grad_N_j[1];
+	double C3 = dt * N_i * grad_N_j[2];
+
+	//SUPG 項
+	double M_s = density * tau * BB_calc_vec3d_dot(v, grad_N_i) * N_j;
+	double A_s = dt * density * tau * BB_calc_vec3d_dot(v, grad_N_i) * BB_calc_vec3d_dot(v, grad_N_j);
+
+	double G_s1 = dt * tau  * BB_calc_vec3d_dot(v, grad_N_i) * grad_N_j[0];
+	double G_s2 = dt * tau  * BB_calc_vec3d_dot(v, grad_N_i) * grad_N_j[1];
+	double G_s3 = dt * tau  * BB_calc_vec3d_dot(v, grad_N_i) * grad_N_j[2];
+
+	//PSPG 項
+	double M_p1 = tau * grad_N_i[0] * N_j;
+	double M_p2 = tau * grad_N_i[1] * N_j;
+	double M_p3 = tau * grad_N_i[2] * N_j;
+
+	double A_p1 = dt * tau * grad_N_i[0] * BB_calc_vec3d_dot(v, grad_N_j);
+	double A_p2 = dt * tau * grad_N_i[1] * BB_calc_vec3d_dot(v, grad_N_j);
+	double A_p3 = dt * tau * grad_N_i[2] * BB_calc_vec3d_dot(v, grad_N_j);
+
+	double G_p = dt * tau * (
+			grad_N_i[0] * grad_N_j[0] +
+			grad_N_i[1] * grad_N_j[1] +
+			grad_N_i[2] * grad_N_j[2]) / density;
+
+	mat[0][0] = M + M_s + (A + A_s + D_11) * 0.5;
+	mat[0][1] = D_12 * 0.5;
+	mat[0][2] = D_13 * 0.5;
+	mat[0][3] = G1 + G_s1;
+	mat[1][0] = D_21 * 0.5;
+	mat[1][1] = M + M_s + (A + A_s + D_22) * 0.5;
+	mat[1][2] = D_23 * 0.5;
+	mat[1][3] = G2 + G_s2;
+	mat[2][0] = D_31 * 0.5;
+	mat[2][1] = D_32 * 0.5;
+	mat[2][2] = M + M_s + (A + A_s + D_33) * 0.5;
+	mat[2][3] = G3 + G_s3;
+	mat[3][0] = C1 + M_p1 + A_p1 * 0.5;
+	mat[3][1] = C2 + M_p2 + A_p2 * 0.5;
+	mat[3][2] = C3 + M_p3 + A_p3 * 0.5;
+	mat[3][3] = G_p;
+}
+
+void BBFE_elemmat_fluid_sups_vec_crank_nicolson(
+		double         vec[4],
+		const double   N_i,
+		const double   grad_N_i[3],
+		const double   v[3],
+		double**       grad_v,
+		const double   density,
+		const double   viscosity,
+		const double   tau,
+		const double   dt,
+		const double*  gravity,
+		double*  surf_tension)
+{
+	for(int d=0; d<3; d++) {
+		double val = 0.0;
+
+		val += density * N_i * v[d]; // M
+
+		val += density * tau * BB_calc_vec3d_dot(v, grad_N_i) * v[d]; // M_s
+
+		val += density * dt * 0.5 * (-N_i) * BB_calc_vec3d_dot(v, grad_v[d]); // A/2
+
+		val += density * dt * 0.5 * (-tau) * BB_calc_vec3d_dot(v, grad_N_i) * BB_calc_vec3d_dot(v, grad_v[d]); // A_s/2
+
+		// D
+		double D_11 = viscosity * ( BB_calc_vec3d_dot(grad_N_i, grad_v[d]) + grad_N_i[0]*grad_v[d][0] );
+		double D_12 = viscosity * grad_N_i[1] * grad_v[d][0];
+		double D_13 = viscosity * grad_N_i[2] * grad_v[d][0];
+		double D_21 = viscosity * grad_N_i[0] * grad_v[d][1];
+		double D_22 = viscosity * ( BB_calc_vec3d_dot(grad_N_i, grad_v[d]) + grad_N_i[1]*grad_v[d][1] );
+		double D_23 = viscosity * grad_N_i[2] * grad_v[d][1];
+		double D_31 = viscosity * grad_N_i[0] * grad_v[d][2];
+		double D_32 = viscosity * grad_N_i[1] * grad_v[d][2];
+		double D_33 = viscosity * ( BB_calc_vec3d_dot(grad_N_i, grad_v[d]) + grad_N_i[2]*grad_v[d][2] );
+
+		if(d==0){
+			val += - density * dt * 0.5 * (D_11 + D_12 + D_13);
+		}else if(d==1){
+			val += - density * dt * 0.5 * (D_21 + D_22 + D_23);
+		}else if(d==2){
+			val += - density * dt * 0.5 * (D_31 + D_32 + D_33);
+		}
+		/* external force */
+		val += density * N_i * gravity[d] * dt;
+		val += surf_tension[d] * dt;
+		val += (density * N_i * gravity[d] * dt + surf_tension[d] * dt) * tau * BB_calc_vec3d_dot(v, grad_N_i);
+
+		vec[d] = val;
+	}
+	
+	vec[3] = 0;
+
+	vec[3] += tau * BB_calc_vec3d_dot(grad_N_i, v); // Mp1
+
+	double A_p1 = tau * grad_N_i[0] * BB_calc_vec3d_dot(v, grad_v[0]);
+	double A_p2 = tau * grad_N_i[1] * BB_calc_vec3d_dot(v, grad_v[1]);
+	double A_p3 = tau * grad_N_i[2] * BB_calc_vec3d_dot(v, grad_v[2]);
+
+	vec[3] += - dt * 0.5 * (A_p1 + A_p2 + A_p3) ; //Ap
+}
--- FE_solvers/mlflow_fs_sups/fluid_elemmat.h
+++ FE_solvers/mlflow_fs_sups/fluid_elemmat.h
@@ -43,3 +43,28 @@ void BBFE_elemmat_fluid_sups_vec(
 		const double   dt,
 		const double*  gravity,
 		double*  surf_tension_vec);
+
+void BBFE_elemmat_fluid_sups_mat_crank_nicolson(
+		double         mat[4][4],
+		const double   N_i,
+		const double   N_j,
+		const double   grad_N_i[3],
+		const double   grad_N_j[3],
+		const double   v[3],
+		const double   density,
+		const double   viscosity,
+		const double   tau,
+		const double   dt);
+
+void BBFE_elemmat_fluid_sups_vec_crank_nicolson(
+		double         vec[4],
+		const double   N_i,
+		const double   grad_N_i[3],
+		const double   v[3],
+		double**       grad_v,
+		const double   density,
+		const double   viscosity,
+		const double   tau,
+		const double   dt,
+		const double*  gravity,
+		double*  surf_tension);
