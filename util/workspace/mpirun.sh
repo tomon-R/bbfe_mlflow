c6e030835f3b5eb9a205823f5523f90f9b9fa531
tomon-R <tomonori.sakuda@outlook.jp>
2025-02-12 16:37:58 +0900

    updated data

--- util/workspace/mpirun.sh
+++ util/workspace/mpirun.sh
@@ -2,4 +2,3 @@ gedatsu_simple_mesh_partitioner -n 8
 gedatsu_bc_partitioner_R -n 8 -ig node.dat -i D_bc_v.dat
 gedatsu_dist_val_partitioner_R -n 8 -ig node.dat -i levelset.dat 
 mpirun -np 8 mlflow_fs_sups ./ | tee output.log
-mpirun -np 2 mlflow_nosc_sups ./ | tee output.log
