e07f8c2f3f1706b8369cd6bd69a62fd074e7aa9c
tomon-R <tomonori.sakuda@outlook.jp>
2025-03-05 21:57:19 +0900

    added shell

--- util/workspace/Dambreak_allnoslip_40_6_40_0.584_0.0876_0.584/execution.sh
+++ util/workspace/Dambreak_allnoslip_40_6_40_0.584_0.0876_0.584/execution.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+mlflow_fs_sups | tee output_1.log
 
-# カラー定義
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-MAGENTA="\e[35m"
-RESET="\e[0m"
+wait
 
-# 実行と色付け
-mlflow_fs_sups | sed "s/^/${RED}/;s/$/${RESET}/" | tee output_1.log &
-mlflow_fs_sups | sed "s/^/${GREEN}/;s/$/${RESET}/" | tee output_2.log &
-mlflow_fs_sups | sed "s/^/${YELLOW}/;s/$/${RESET}/" | tee output_3.log &
-mlflow_fs_sups | sed "s/^/${BLUE}/;s/$/${RESET}/" | tee output_4.log &
-mlflow_fs_sups | sed "s/^/${MAGENTA}/;s/$/${RESET}/" | tee output_5.log &
+mlflow_fs_sups | tee output_2.log
 
-# 全プロセスの終了待ち
+wait
+
+mlflow_fs_sups | tee output_3.log
+
+wait
+
+mlflow_fs_sups | tee output_4.log
+
+wait
+
+mlflow_fs_sups | tee output_5.log
+
 wait
