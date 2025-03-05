9b90d36aac5c7dc8a4d34369aa34584df46d1bb6
tomon-R <tomonori.sakuda@outlook.jp>
2025-03-05 22:06:58 +0900

    added shell

--- util/workspace/Dambreak_allnoslip_40_6_40_0.584_0.0876_0.584/execution.sh
+++ util/workspace/Dambreak_allnoslip_40_6_40_0.584_0.0876_0.584/execution.sh
@@ -1,19 +1,18 @@
 mlflow_fs_sups | tee output_1.log
-
 wait
+rm result*
 
 mlflow_fs_sups | tee output_2.log
-
 wait
+rm result*
 
 mlflow_fs_sups | tee output_3.log
-
 wait
+rm result*
 
 mlflow_fs_sups | tee output_4.log
-
 wait
+rm result*
 
 mlflow_fs_sups | tee output_5.log
-
 wait
