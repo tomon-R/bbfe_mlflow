2161de9f57862003f8fe0126c9cda4f24f10a863
tomon-R <tomonori.sakuda@outlook.jp>
2025-03-05 21:55:07 +0900

    added shell

--- util/workspace/Dambreak_allnoslip_40_6_40_0.584_0.0876_0.584/execution.sh
+++ util/workspace/Dambreak_allnoslip_40_6_40_0.584_0.0876_0.584/execution.sh
@@ -1,9 +1,19 @@
-mlflow_fs_sups | tee output_1.log
+#!/bin/bash
 
-mlflow_fs_sups | tee output_2.log
+# カラー定義
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+RESET="\e[0m"
 
-mlflow_fs_sups | tee output_3.log
+# 実行と色付け
+mlflow_fs_sups | sed "s/^/${RED}/;s/$/${RESET}/" | tee output_1.log &
+mlflow_fs_sups | sed "s/^/${GREEN}/;s/$/${RESET}/" | tee output_2.log &
+mlflow_fs_sups | sed "s/^/${YELLOW}/;s/$/${RESET}/" | tee output_3.log &
+mlflow_fs_sups | sed "s/^/${BLUE}/;s/$/${RESET}/" | tee output_4.log &
+mlflow_fs_sups | sed "s/^/${MAGENTA}/;s/$/${RESET}/" | tee output_5.log &
 
-mlflow_fs_sups | tee output_4.log
-
-mlflow_fs_sups | tee output_5.log
+# 全プロセスの終了待ち
+wait
